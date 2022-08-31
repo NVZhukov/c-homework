eee9b89d9470613e0410fc159588b1cff0824242
Zhukov Nikita <nypno@mail.ru>
2022-08-31 20:17:44 +0300

    added dicision third task

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.WriteLine("Введите целое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x % 2 == 0)
+    {
+        Console.WriteLine("Число " + x + " является четным");
+    }
+    else
+    {
+        Console.WriteLine("Число " + x + " является нечетным");
+    }
+}
+catch
+{
+    Console.WriteLine("Попробуйте ввести корректные данные");
+}
