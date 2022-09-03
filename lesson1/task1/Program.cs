1c892fb0cfdd54d2a0d7892ae6e270b91cfdf649
Zhukov Nikita <nypno@mail.ru>
2022-09-03 18:28:22 +0300

    moved everythink to a folder lesson1

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    Console.WriteLine("Введите целое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите еще одно целое число");
+    int y = Convert.ToInt32(Console.ReadLine());
+    if (x > y)
+    {
+        Console.WriteLine("max = " + x);
+    }
+    else if (y > x)
+    {
+        Console.WriteLine("max = " + y);
+    }
+    else
+    {
+        Console.WriteLine("Числа " + x + " и " + y + " равны");
+    }
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целые числа!");
+}
---
+++ lesson1/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.WriteLine("Введите целое число");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int k = 1;
+    while (k <= n)
+    {
+        if (k % 2 == 0)
+        {
+            Console.Write(k + ", ");
+        }
+        k++;
+    }
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целое число!");
+}
+
