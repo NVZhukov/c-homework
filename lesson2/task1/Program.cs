fbf3b6ec70922aa320946d1d4cb6f6bdcba33dc6
Zhukov Nikita <nypno@mail.ru>
2022-09-03 18:34:25 +0300

    solved task 1

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void secondDigit()
+{
+    Console.WriteLine("Введите целое трехзначное число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 99 && x < 1000)
+    {
+        int x1 = x % 100;
+        int x2 = x1 / 10;
+        Console.WriteLine("Вторая цифра введенного числа: " + x2);
+    }
+    else
+    {
+        Console.WriteLine("Необходимо трехзначное число!");
+    }
+}
+
+try
+{
+    secondDigit();
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные!");
+}
+
+
