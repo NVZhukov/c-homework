c264b0b3be98e3d15566b8028b3bce374d37876e
Zhukov Nikita <nypno@mail.ru>
2022-09-04 16:05:48 +0300

    solved task 2

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    Console.WriteLine("Введите целое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    if (x > 99)
+    {
+        int x2 = x;
+        while (x2 >= 1000) x2 /= 10;
+        int x1 = x2 % 10;
+        Console.WriteLine($"У числа {x} третья цифра - {x1}");
+    }
+    else
+    {
+        Console.WriteLine($"У числа {x} нет третьей цифры");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные!");
+}
