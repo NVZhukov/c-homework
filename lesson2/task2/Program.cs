dd544d33810a9a72e2860130c7ac8b606d885843
Zhukov Nikita <nypno@mail.ru>
2022-09-04 16:09:01 +0300

    corrected task 2

--- lesson2/task2/Program.cs
+++ lesson2/task2/Program.cs
@@ -6,7 +6,10 @@
     if (x > 99)
     {
         int x2 = x;
-        while (x2 >= 1000) x2 /= 10;
+        while (x2 >= 1000)
+        {
+            x2 /= 10;
+        }
         int x1 = x2 % 10;
         Console.WriteLine($"У числа {x} третья цифра - {x1}");
     }
