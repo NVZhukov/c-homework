53ee41af77856e3b5bd12164179aae241e55cafd
Zhukov Nikita <nypno@mail.ru>
2022-09-15 17:59:24 +0300

    modified task3 solve

--- lesson5/task3/Program.cs
+++ lesson5/task3/Program.cs
@@ -20,21 +20,6 @@ void PrintArray(double[] mas)
     Console.WriteLine(mas[mas.Length - 1] + "]");
 }
 
-// double MaxMinDiff(double[] array)
-// {
-//     double min = array[0];
-//     double max = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (min > array[i]) min = array[i];
-//     }
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (max < array[i]) max = array[i];
-//     }
-//     return Math.Round(max - min, 2);
-// }
-
 double[] MaxMinDiff(double[] array)
 {
     double[] diff = new double[3];
@@ -60,9 +45,16 @@ double[] MaxMinDiff(double[] array)
     return diff;
 }
 
-System.Console.WriteLine("Введите размерность массива");
-int k = Convert.ToInt32(Console.ReadLine());
-double[] array = FillArray(k);
-PrintArray(array);
-double[] mas = MaxMinDiff(array);
-Console.WriteLine($"Разница между максимальным ({mas[1]}) и минимальным ({mas[0]}) элементами массива = {mas[2]}");
+try
+{
+    System.Console.WriteLine("Введите размерность массива");
+    int k = Convert.ToInt32(Console.ReadLine());
+    double[] array = FillArray(k);
+    PrintArray(array);
+    double[] mas = MaxMinDiff(array);
+    Console.WriteLine($"Разница между максимальным ({mas[1]}) и минимальным ({mas[0]}) элементами массива = {mas[2]}");
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целое положительное число!");
+}
