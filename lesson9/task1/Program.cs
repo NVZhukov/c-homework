5a899cb7c3fbff2684dcdcd450dc4fe56f9e1f05
NVZhukov <nypno@mail.ru>
2022-10-02 22:36:32 +0300

    solved 2 tasks

---
+++ lesson9/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+string SeriesNumber(int N)
+{
+    //if (N == 0) return "\"";
+    if (N == 1) return Convert.ToString(N);
+    return N + ", " + SeriesNumber(N - 1);
+}
+
+try
+{
+    Console.Write("Введите натуральное число N = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"N = {n} -> \"{SeriesNumber(n)}\"");
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целое положительное число!");
+}
---
+++ lesson9/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNum(int M, int N)
+{
+    if (M == N) return M;
+    return N + SumNum(M, N - 1);
+}
+
+try
+{
+    Console.WriteLine("Считаем сумму натуральных чисел от M до N");
+    Console.Write("Введите натуральное число M = ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите натуральное число N = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"M = {m}; N = {n} -> {SumNum(m, n)}");
+}
+catch
+{
+    Console.WriteLine("Надо было вводить целые положительные числа!");
+}
